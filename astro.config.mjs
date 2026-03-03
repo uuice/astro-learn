3cc76c1e72019dde2fc01c46d1b7ffe20fcd73c2
yujiajia <yujiajia@asteria.com>
2026-03-03 18:54:08 +0800

    同步

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,32 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import remarkToc from 'remark-toc'
+import rehypeSlug from 'rehype-slug'
 import rehypePresetMinify from 'rehype-preset-minify'
 import tailwindcss from '@tailwindcss/vite'
 import node from '@astrojs/node'
 import react from '@astrojs/react'
-// https://astro.build/config
+
+const rehypePluginsBase = [rehypeSlug]
+
 export default defineConfig({
   markdown: {
-      syntaxHighlight: 'prism',
-      remarkPlugins: [[remarkToc, { heading: 'toc', maxDepth: 3 } ]],
-      gfm: true,
+    syntaxHighlight: 'shiki',
+    shikiConfig: { theme: 'github-dark', wrap: true },
+    remarkPlugins: [[remarkToc, { heading: '目录', maxDepth: 3 }]],
+    rehypePlugins: [...rehypePluginsBase],
+    gfm: true,
   },
 
   integrations: [mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: { theme: 'dracula' },
-      remarkPlugins: [[remarkToc, { heading: 'toc', maxDepth: 3 } ]],
-      rehypePlugins: [rehypePresetMinify],
-      remarkRehype: { footnoteLabel: 'Footnotes' },
-      gfm: false, 
-      extendMarkdownConfig: true
-  }
-  ), react()],
+    syntaxHighlight: 'shiki',
+    shikiConfig: { theme: 'github-dark', wrap: true },
+    remarkPlugins: [[remarkToc, { heading: '目录', maxDepth: 3 }]],
+    rehypePlugins: [...rehypePluginsBase, rehypePresetMinify],
+    remarkRehype: { footnoteLabel: 'Footnotes' },
+    gfm: true,
+    extendMarkdownConfig: true,
+  }), react()],
 
   vite: {
     plugins: [tailwindcss()],
